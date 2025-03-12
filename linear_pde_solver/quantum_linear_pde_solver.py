28e6b7dd7ba23eff41574d8482e13f0798005247
farmerp <farmerp@tcd.ie>
2025-03-12 15:25:04 +0000

    Added api key to git ignore

--- linear_pde_solver/quantum_linear_pde_solver.py
+++ linear_pde_solver/quantum_linear_pde_solver.py
@@ -1,6 +1,6 @@
 import numpy as np
 from qiskit import QuantumCircuit, transpile
-from qiskit_aer import AerSimulator, Aer
+from qiskit_aer import AerSimulator
 
 class QuantumLinearPDESolver:
     def __init__(self, a, b, c, f, x_range, t_range, nx, nt):
@@ -15,7 +15,7 @@ class QuantumLinearPDESolver:
         self.dx = (x_range[1] - x_range[0]) / nx
         self.dt = (t_range[1] - t_range[0]) / nt
         self.u = np.zeros((nt+1, nx+1))
-        self.backend = AerSimulator()
+        self.backend = AerSimulator(method="statevector")
 
     def initial_conditions(self, u0):
         self.u[0, :] = u0
@@ -25,35 +25,35 @@ class QuantumLinearPDESolver:
         self.u[:, -1] = right_bc
 
     def solve(self):
-        for n in range(0, self.nt):
+        for n in range(self.nt):
             for i in range(1, self.nx):
-                self.u[n+1, i] = self.run_quantum_circuit(n, i)
+                self.u[n+1, i] = self.run_quantum_circuit(self.u[n, i-1], self.u[n, i], self.u[n, i+1])
 
-    def run_quantum_circuit(self, n, i):
-        qc = QuantumCircuit(3, 3)
-        
-        # Encode the input state
+    def run_quantum_circuit(self, u_left, u_center, u_right):
+        qc = QuantumCircuit(3)
+        values = np.array([u_left, u_center, u_right], dtype=complex)
+        norm = np.linalg.norm(values)
+        if norm < 1e-12:
+            values = np.zeros_like(values)
+            values[0] = 1.0
+        else:
+            values /= norm
+        init_state = np.zeros(8, dtype=complex)
+        init_state[0] = values[0]
+        init_state[1] = values[1]
+        init_state[2] = values[2]
+        qc.initialize(init_state, [0, 1, 2])
         qc.h(0)
         qc.cx(0, 1)
-        qc.cx(0, 2)
-        
-        # Apply the PDE coefficients as quantum gates
-        qc.u(self.a * self.dt / self.dx**2, 0, 0, 0)
-        qc.u(self.b * self.dt / (2 * self.dx), 0, 0, 1)
-        qc.u(self.c * self.dt, 0, 0, 2)
-        
-        # Measure the qubits
-        qc.measure([0, 1, 2], [0, 1, 2])
-        
-        # Execute the circuit
-        qc_compiled = transpile(qc, backend=self.backend)
-        job = self.backend.run(qc_compiled) 
-        result = job.result()
-        counts = result.get_counts(qc)
-        
-        # Decode the result
-        measured_value = max(counts, key=counts.get)
-        return int(measured_value, 2) * self.dt
+        qc.cx(1, 2)
+        qc.rx(self.a * self.dt / self.dx**2, 0)
+        qc.ry(self.b * self.dt / (2*self.dx), 1)
+        qc.rz(self.c * self.dt, 2)
+        qc.save_statevector()
+        result = self.backend.run(qc).result()
+        final_state = result.get_statevector(qc)
+        return (final_state[1] * norm).real
+
 
     def get_solution(self):
         return self.u
