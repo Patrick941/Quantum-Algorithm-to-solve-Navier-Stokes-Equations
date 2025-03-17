6dbd5cc22aa50873107092c91d4c35d336db2224
farmerp <farmerp@tcd.ie>
2025-03-17 13:22:06 +0000

    Added VQA scripts

--- linear_pde_solver/quantum_linear_pde_solver.py
+++ linear_pde_solver/quantum_linear_pde_solver.py
@@ -1,102 +1,316 @@
-
-
-import time
-
+import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import Aer, transpile, assemble
+import math
+import random
 import numpy as np
+from scipy.optimize import minimize
+import os
 import matplotlib.pyplot as plt
-from tqdm.auto import tqdm
-from qiskit import QuantumCircuit, Aer
-from qiskit.aqua import QuantumInstance
 
-from vqa_poisson import VQAforPoisson
 
-def experiment(bc, num_trials, num_qubits_list, num_layers, qins):
-    
-    print('-----------'+bc+' boundary condition --------------')
-    
-    data = {'num_qubits':[], 'obj_count':[], 'circ_count':[], 'iter_count':[], 'err':[], 'params':[], 'q_sol':[], 'cl_sol':[]}
-    
-    for num_qubits in tqdm(num_qubits_list):
-        print('-------------------------')
-        print('num_qubits:', num_qubits)
-        
-        # set oracle for f vector
-        oracle_f = QuantumCircuit(num_qubits)
-        oracle_f.x(num_qubits-1)
-        oracle_f.h(oracle_f.qubits)
-    
-        # set vqa instance
-        vqa = VQAforPoisson(num_qubits, num_layers, bc, oracle_f=oracle_f, qinstance=qins)
+def apply_fixed_ansatz(qubits, parameters):
 
-        obj_counts = []
-        circ_counts = []
-        iter_counts = []
-        err = []
-        params = []
-        q_sol = []
+    for iz in range (0, len(qubits)):
+        circ.ry(parameters[0][iz], qubits[iz])
+
+    circ.cz(qubits[0], qubits[1])
+    circ.cz(qubits[2], qubits[0])
+
+    for iz in range (0, len(qubits)):
+        circ.ry(parameters[1][iz], qubits[iz])
+
+    circ.cz(qubits[1], qubits[2])
+    circ.cz(qubits[2], qubits[0])
+
+    for iz in range (0, len(qubits)):
+        circ.ry(parameters[2][iz], qubits[iz])
+
+def had_test(gate_type, qubits, auxiliary_index, parameters):
+
+    circ.h(auxiliary_index)
+
+    apply_fixed_ansatz(qubits, parameters)
+
+    for ie in range (0, len(gate_type[0])):
+        if (gate_type[0][ie] == 1):
+            circ.cz(auxiliary_index, qubits[ie])
+
+    for ie in range (0, len(gate_type[1])):
+        if (gate_type[1][ie] == 1):
+            circ.cz(auxiliary_index, qubits[ie])
     
-        for seed in range(num_trials):
-        
-            np.random.seed(seed)
-        
-            x0 = list(4*np.pi*np.random.rand(vqa.num_params))
-            res = vqa.minimize(x0, method='bfgs', save_logs=True)
+    circ.h(auxiliary_index)
     
-            obj_counts.append(vqa.objective_counts)
-            circ_counts.append(vqa.circuit_counts)
-            iter_counts.append(len(vqa.objective_count_logs))
-            err.append(vqa.get_errors(res['x'])['trace'])
-            params.append(res['x'])
-            q_sol.append(vqa.get_sol(res['x']).real)
+
+def control_fixed_ansatz(qubits, parameters, auxiliary, reg):
+
+    for i in range (0, len(qubits)):
+        circ.cry(parameters[0][i], qiskit.circuit.Qubit(reg, auxiliary), qiskit.circuit.Qubit(reg, qubits[i]))
+
+    circ.ccx(auxiliary, qubits[1], 4)
+    circ.cz(qubits[0], 4)
+    circ.ccx(auxiliary, qubits[1], 4)
+
+    circ.ccx(auxiliary, qubits[0], 4)
+    circ.cz(qubits[2], 4)
+    circ.ccx(auxiliary, qubits[0], 4)
+
+    for i in range (0, len(qubits)):
+        circ.cry(parameters[1][i], qiskit.circuit.Qubit(reg, auxiliary), qiskit.circuit.Qubit(reg, qubits[i]))
+
+    circ.ccx(auxiliary, qubits[2], 4)
+    circ.cz(qubits[1], 4)
+    circ.ccx(auxiliary, qubits[2], 4)
+
+    circ.ccx(auxiliary, qubits[0], 4)
+    circ.cz(qubits[2], 4)
+    circ.ccx(auxiliary, qubits[0], 4)
+
+    for i in range (0, len(qubits)):
+        circ.cry(parameters[2][i], qiskit.circuit.Qubit(reg, auxiliary), qiskit.circuit.Qubit(reg, qubits[i]))
+
+
+def control_b(auxiliary, qubits):
+
+    for ia in qubits:
+        circ.ch(auxiliary, ia)
+
+
+def special_had_test(gate_type, qubits, auxiliary_index, parameters, reg):
+
+    circ.h(auxiliary_index)
+
+    control_fixed_ansatz(qubits, parameters, auxiliary_index, reg)
+
+    for ty in range (0, len(gate_type)):
+        if (gate_type[ty] == 1):
+            circ.cz(auxiliary_index, qubits[ty])
+
+
+    control_b(auxiliary_index, qubits)
+    
+    circ.h(auxiliary_index)
+
+def calculate_cost_function(parameters):
+    
+    global opt
+
+    overall_sum_1 = 0
+    
+    parameters = [parameters[0:3], parameters[3:6], parameters[6:9]]
+
+    for i in range(0, len(gate_set)):
+        for j in range(0, len(gate_set)):
+
+            global circ
+
+            qctl = QuantumRegister(5)
+            qc = ClassicalRegister(5)
+            circ = QuantumCircuit(qctl, qc)
+
+            backend = Aer.get_backend('aer_simulator')
             
-            print('trial:', seed, 'Err.:', err[-1])
-        
-        data['num_qubits'].append(num_qubits)
-        data['obj_count'].append(obj_counts)
-        data['circ_count'].append(circ_counts)
-        data['iter_count'].append(iter_counts)
-        data['err'].append(err)
-        data['params'].append(params)
-        data['q_sol'].append(q_sol)
-        data['cl_sol'].append(vqa.get_cl_sol().real)
-        
-    return data
+            multiply = coefficient_set[i]*coefficient_set[j]
 
-t0 = time.time()
-optimizer = 'bfgs'
-num_layers = 5
-num_trials = 1
-num_qubits_list = [4]
-qins = QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=42)
+            had_test([gate_set[i], gate_set[j]], [1, 2, 3], 0, parameters)
 
-# bc: Periodic
-data_p = experiment('Periodic', num_trials, num_qubits_list, num_layers, qins)
+            circ.save_statevector()
+            t_circ = transpile(circ, backend)
+            qobj = assemble(t_circ)
+            job = backend.run(qobj)
+
+            result = job.result()
+            outputstate = np.real(result.get_statevector(circ, decimals=100))
+            o = outputstate
+
+            m_sum = 0
+            for l in range (0, len(o)):
+                if (l%2 == 1):
+                    n = o[l]**2
+                    m_sum+=n
+
+            overall_sum_1+=multiply*(1-(2*m_sum))
+
+    overall_sum_2 = 0
+
+    for i in range(0, len(gate_set)):
+        for j in range(0, len(gate_set)):
+
+            multiply = coefficient_set[i]*coefficient_set[j]
+            mult = 1
+
+            for extra in range(0, 2):
+
+                qctl = QuantumRegister(5)
+                qc = ClassicalRegister(5)
+                circ = QuantumCircuit(qctl, qc)
+
+                backend = Aer.get_backend('aer_simulator')
+
+                if (extra == 0):
+                    special_had_test(gate_set[i], [1, 2, 3], 0, parameters, qctl)
+                if (extra == 1):
+                    special_had_test(gate_set[j], [1, 2, 3], 0, parameters, qctl)
+
+                circ.save_statevector()    
+                t_circ = transpile(circ, backend)
+                qobj = assemble(t_circ)
+                job = backend.run(qobj)
+
+                result = job.result()
+                outputstate = np.real(result.get_statevector(circ, decimals=100))
+                o = outputstate
+
+                m_sum = 0
+                for l in range (0, len(o)):
+                    if (l%2 == 1):
+                        n = o[l]**2
+                        m_sum+=n
+                mult = mult*(1-(2*m_sum))
+
+            overall_sum_2+=multiply*mult
+            
+    print(1-float(overall_sum_2/overall_sum_1))
+
+    return 1-float(overall_sum_2/overall_sum_1)
 
 
-def plot_solution_vectors(q_sol, cl_sol):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(q_sol, label='quantum', color='black')
-    ax.plot(cl_sol, label='classical', color='black', linestyle='dashed')
-    ax.legend()
-    ax.set_xlabel('Node number')
-    ax.set_ylabel('Components of solution')
-
-    cnorm = np.linalg.norm(q_sol)
-    qnorm = np.linalg.norm(cl_sol)
-
-    ax.text(0.55, 0.65, 'Norm (quantum) = %.1f'%(qnorm), transform=ax.transAxes)
-    ax.text(0.55, 0.55, 'Norm (classical) = %.1f'%(cnorm), transform=ax.transAxes)
+def calculate_cost_function(parameters):
     
-    return fig, ax
+    global opt
 
-idx1, idx2 = 0, 0
-print('Periodic boundary condition, num_qubits:', data_p['num_qubits'][idx1])
-q_sol = data_p['q_sol'][idx1][idx2]
-cl_sol = data_p['cl_sol'][idx1]
-plot_solution_vectors(q_sol, cl_sol)
+    overall_sum_1 = 0
+    
+    parameters = [parameters[0:3], parameters[3:6], parameters[6:9]]
+
+    for i in range(0, len(gate_set)):
+        for j in range(0, len(gate_set)):
+
+            global circ
+
+            qctl = QuantumRegister(5)
+            qc = ClassicalRegister(5)
+            circ = QuantumCircuit(qctl, qc)
+
+            backend = Aer.get_backend('aer_simulator')
+            
+            multiply = coefficient_set[i]*coefficient_set[j]
+
+            had_test([gate_set[i], gate_set[j]], [1, 2, 3], 0, parameters)
+
+            circ.save_statevector()
+            t_circ = transpile(circ, backend)
+            qobj = assemble(t_circ)
+            job = backend.run(qobj)
+
+            result = job.result()
+            outputstate = np.real(result.get_statevector(circ, decimals=100))
+            o = outputstate
+
+            m_sum = 0
+            for l in range (0, len(o)):
+                if (l%2 == 1):
+                    n = o[l]**2
+                    m_sum+=n
+
+            overall_sum_1+=multiply*(1-(2*m_sum))
+
+    overall_sum_2 = 0
+
+    for i in range(0, len(gate_set)):
+        for j in range(0, len(gate_set)):
+
+            multiply = coefficient_set[i]*coefficient_set[j]
+            mult = 1
+
+            for extra in range(0, 2):
+
+                qctl = QuantumRegister(5)
+                qc = ClassicalRegister(5)
+                circ = QuantumCircuit(qctl, qc)
+
+                backend = Aer.get_backend('aer_simulator')
+
+                if (extra == 0):
+                    special_had_test(gate_set[i], [1, 2, 3], 0, parameters, qctl)
+                if (extra == 1):
+                    special_had_test(gate_set[j], [1, 2, 3], 0, parameters, qctl)
+
+                circ.save_statevector()    
+                t_circ = transpile(circ, backend)
+                qobj = assemble(t_circ)
+                job = backend.run(qobj)
+
+                result = job.result()
+                outputstate = np.real(result.get_statevector(circ, decimals=100))
+                o = outputstate
+
+                m_sum = 0
+                for l in range (0, len(o)):
+                    if (l%2 == 1):
+                        n = o[l]**2
+                        m_sum+=n
+                mult = mult*(1-(2*m_sum))
+
+            overall_sum_2+=multiply*mult
+            
+    print(1-float(overall_sum_2/overall_sum_1))
+
+    return 1-float(overall_sum_2/overall_sum_1)
+
+coefficient_set = [2.0]  # A = 2I
+gate_set = [[0, 0, 0]]   # Identity gate on all qubits
+
+# b remains the uniform superposition
+b = np.array([1/np.sqrt(8)]*8)
+
+# Run VQLS optimization
+out = minimize(calculate_cost_function, x0=[float(random.randint(0,3000))/1000 for i in range(0, 9)], method="COBYLA", options={'maxiter':200})
+print(out)
+
+# Post-process solution
+out_f = [out['x'][0:3], out['x'][3:6], out['x'][6:9]]
+circ = QuantumCircuit(3)
+apply_fixed_ansatz([0, 1, 2], out_f)
+circ.save_statevector()
+
+backend = Aer.get_backend('aer_simulator')
+t_circ = transpile(circ, backend)
+qobj = assemble(t_circ)
+job = backend.run(qobj)
+
+result = job.result()
+o = result.get_statevector(circ, decimals=10)
+
+# Verify solution (A = 2I)
+a = 2.0 * np.eye(8)  # A is 2I
+b_norm = b / np.linalg.norm(b)
+x = o / np.linalg.norm(o)
+overlap = np.abs(b_norm.dot(a.dot(x)))**2
+print("Overlap with exact solution:", overlap)
 
 
-print('elapsed time: %.2e'%(time.time() - t0))
+# Extract the quantum solution and scale by 1/2 (since A = 2I)
+x_quantum = np.real(o) / 2  # Use real parts (imaginary parts should be ~0)
 
+# Exact solution: x = A^{-1}b = b/2 (since A = 2I)
+x_exact = np.array([1/(2*np.sqrt(8))] * 8)  # Uniform values
+
+# Create grid points (1D domain)
+grid_points = np.arange(8)  # 0 to 7
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.plot(grid_points, x_quantum, 'bo', markersize=8, label='Quantum Solution')
+plt.plot(grid_points, x_exact, 'r--', linewidth=2, label='Exact Solution')
+plt.xlabel('Grid Point', fontsize=12)
+plt.ylabel('Solution Value', fontsize=12)
+plt.xticks(grid_points)
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.legend()
+plt.title('1D Poisson Equation: $-\Delta u = f$ (Solution)', fontsize=14)
+if not os.path.exists(os.path.join(os.path.dirname(__file__), 'Images')):
+    os.makedirs(os.path.join(os.path.dirname(__file__), 'Images'))
+
+plt.savefig(os.path.join(os.path.dirname(__file__), 'Images', 'quantum_solution.png'))
