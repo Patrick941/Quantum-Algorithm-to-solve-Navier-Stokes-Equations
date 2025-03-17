e43f363ba65638d3848fa8adb059608d6cd5b153
farmerp <farmerp@tcd.ie>
2025-03-17 21:39:23 +0000

    |||

--- linear_pde_solver/quantum_linear_pde_solver.py
+++ linear_pde_solver/quantum_linear_pde_solver.py
@@ -2,60 +2,99 @@ from qiskit import QuantumRegister, QuantumCircuit, Aer, execute
 import numpy as np
 
 class HHLAlgorithm:
-    def __init__(self, t=2, a=1, b=-1/3, theta=0):
-        self.t = t
-        self.a = a
-        self.b = b
-        self.theta = theta
-        self.num_qubits = 4
-        self.nb = 1  # Solution qubits
-        self.nl = 2  # Eigenvalue qubits
+    def __init__(self, matrix_params, algorithm_params, state_prep_params):
+        """
+        Initialize HHL algorithm with parameters
         
+        :param matrix_params: Dictionary with keys 'a', 'b' (matrix elements)
+        :param algorithm_params: Dictionary with keys 't', 'nl', 'nb'
+        :param state_prep_params: Dictionary with key 'theta'
+        """
+        # Matrix parameters
+        self.a = matrix_params.get('a', 1)
+        self.b = matrix_params.get('b', -1/3)
+        
+        # Algorithm parameters
+        self.t = algorithm_params.get('t', 2)
+        self.nl = algorithm_params.get('nl', 2)  # Eigenvalue qubits
+        self.nb = algorithm_params.get('nb', 1)  # Solution qubits
+        self.na = 1  # Ancilla qubits
+        
+        # State preparation parameters
+        self.theta = state_prep_params.get('theta', 0)
+        
+        # Derived parameters
+        self.num_qubits = self.nb + self.nl + self.na
+        self._calculate_eigenvalues()
+        self._calculate_rotation_angles()
+        
+        # Initialize quantum circuit
         self.qr = QuantumRegister(self.num_qubits)
         self.qc = QuantumCircuit(self.qr)
         self._build_circuit()
+
+    def _calculate_eigenvalues(self):
+        """Calculate eigenvalues of the matrix A = [[a, b], [b, a]]"""
+        self.lambda1 = self.a + self.b
+        self.lambda2 = self.a - self.b
+
+    def _calculate_rotation_angles(self):
+        """Calculate angles for eigenvalue rotation based on current parameters"""
+        # Calculate arcsin terms using matrix parameters
+        self.arcsin_term = 2 * np.arcsin(abs(self.b))
         
+        # Calculate rotation angles
+        self.rot_angles = [
+            (-np.pi + np.pi/3 - self.arcsin_term)/4,
+            (-np.pi - np.pi/3 + self.arcsin_term)/4,
+            (np.pi - np.pi/3 - self.arcsin_term)/4,
+            (np.pi + np.pi/3 + self.arcsin_term)/4
+        ]
+
     def _build_circuit(self):
-        # Define register slices
+        """Construct the quantum circuit"""
         qrb = self.qr[0:self.nb]
         qrl = self.qr[self.nb:self.nb+self.nl]
-        qra = self.qr[self.nb+self.nl:self.nb+self.nl+1]
+        qra = self.qr[self.nb+self.nl:self.nb+self.nl+self.na]
 
         # State preparation
         self.qc.ry(2*self.theta, qrb[0])
 
         # Quantum Phase Estimation
+        self._apply_qpe(qrl, qrb)
+        
+        # Inverse QFT
+        self._inverse_qft(qrl)
+        
+        # Eigenvalue rotation
+        self._apply_eigenvalue_rotation(qrl, qra)
+        
+        self.qc.measure_all()
+
+    def _apply_qpe(self, qrl, qrb):
+        """Apply Quantum Phase Estimation"""
         for qu in qrl:
             self.qc.h(qu)
             
         self.qc.p(self.a*self.t, qrl[0])
         self.qc.p(self.a*self.t*2, qrl[1])
         self.qc.u(self.b*self.t, -np.pi/2, np.pi/2, qrb[0])
-
+        
         # Controlled rotations
-        self._add_controlled_rotations(qrl, qrb)
-        
-        # Inverse QFT
-        self._inverse_qft(qrl)
-
-        # Eigenvalue rotation
-        self._eigenvalue_rotation(qrl, qra)
-        
-        self.qc.measure_all()
-
-    def _add_controlled_rotations(self, qrl, qrb):
-        # Helper method for controlled rotation blocks
         for i, factor in enumerate([1, 2]):
-            params = self.b * self.t * factor
-            self.qc.p(np.pi/2, qrb[0])
-            self.qc.cx(qrl[i], qrb[0])
-            self.qc.ry(params, qrb[0])
-            self.qc.cx(qrl[i], qrb[0])
-            self.qc.ry(-params, qrb[0])
-            self.qc.p(3*np.pi/2, qrb[0])
+            self._controlled_rotation(qrl[i], qrb[0], self.b*self.t*factor)
+
+    def _controlled_rotation(self, control, target, params):
+        """Generic controlled rotation block"""
+        self.qc.p(np.pi/2, target)
+        self.qc.cx(control, target)
+        self.qc.ry(params, target)
+        self.qc.cx(control, target)
+        self.qc.ry(-params, target)
+        self.qc.p(3*np.pi/2, target)
 
     def _inverse_qft(self, qrl):
-        # Inverse Quantum Fourier Transform
+        """Apply Inverse Quantum Fourier Transform"""
         self.qc.h(qrl[1])
         self.qc.rz(-np.pi/4, qrl[1])
         self.qc.cx(qrl[0], qrl[1])
@@ -64,38 +103,46 @@ class HHLAlgorithm:
         self.qc.rz(-np.pi/4, qrl[0])
         self.qc.h(qrl[0])
 
-    def _eigenvalue_rotation(self, qrl, qra):
-        # Eigenvalue rotation parameters
-        t1 = (-np.pi + np.pi/3 - 2*np.arcsin(1/3))/4
-        t2 = (-np.pi - np.pi/3 + 2*np.arcsin(1/3))/4
-        t3 = (np.pi - np.pi/3 - 2*np.arcsin(1/3))/4
-        t4 = (np.pi + np.pi/3 + 2*np.arcsin(1/3))/4
-
-        # Conditional rotations
-        rotations = [(qrl[1], t1), (qrl[0], t2), 
-                    (qrl[1], t3), (qrl[0], t4)]
-        
-        for control_qubit, angle in rotations:
+    def _apply_eigenvalue_rotation(self, qrl, qra):
+        """Apply conditional eigenvalue rotation"""
+        controls = [qrl[1], qrl[0], qrl[1], qrl[0]]
+        for control_qubit, angle in zip(controls, self.rot_angles):
             self.qc.cx(control_qubit, qra[0])
             self.qc.ry(angle, qra[0])
 
     def run(self, shots=1024):
+        """Execute the circuit"""
         simulator = Aer.get_backend('qasm_simulator')
         result = execute(self.qc, simulator, shots=shots).result()
         return result.get_counts()
 
 def main():
-    # Initialize and run HHL algorithm
-    hhl = HHLAlgorithm(t=2)
+    # Your PDE parameters
+    matrix_params = {
+        'a': 1,      # Diagonal element
+        'b': -1/3    # Off-diagonal element (matches discretization)
+    }
+
+    algorithm_params = {
+        't': 3*np.pi/4,  # Optimal for eigenvalues 2/3 and 4/3
+        'nl': 2,          # Eigenvalue qubits
+        'nb': 1           # Solution qubits
+    }
+
+    state_prep_params = {
+        'theta': 0        # Initial state |0>
+    }
+
+    # Initialize HHL
+    hhl = HHLAlgorithm(matrix_params, algorithm_params, state_prep_params)
     
-    # Print circuit info
-    print(f"Circuit depth: {hhl.qc.depth()}")
-    print(f"CNOT count: {hhl.qc.count_ops().get('cx', 0)}")
-    
-    # Run simulation and show results
-    counts = hhl.run()
-    print("\nMeasurement results:")
-    print(counts)
+    counts = hhl.run(shots=1000)
+
+    # Filter valid solutions (ancilla=1)
+    valid_results = {k:v for k,v in counts.items() if k[-1] == '1'}
+
+    print("Valid solutions (ancilla=1):")
+    print(valid_results)
 
 if __name__ == "__main__":
     main()
