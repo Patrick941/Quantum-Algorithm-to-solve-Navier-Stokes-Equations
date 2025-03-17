ae0d1f1b56a7a8bd2f34c0c35cbc2bf041b372e1
farmerp <farmerp@tcd.ie>
2025-03-17 13:26:35 +0000

    Added VQA scripts

--- linear_pde_solver/VQAPoisson/sample.py
+++ linear_pde_solver/VQAPoisson/sample.py
@@ -8,32 +8,48 @@ from qiskit.aqua import QuantumInstance
 import os
 from vqa_poisson import VQAforPoisson
 
+
 def experiment(bc, num_trials, num_qubits_list, num_layers, qins):
-    print('-----------' + bc + ' boundary condition --------------')
-    data = {'num_qubits': [], 'obj_count': [], 'circ_count': [], 'iter_count': [], 'err': [], 'params': [], 'q_sol': [], 'cl_sol': []}
+    
+    print('-----------'+bc+' boundary condition --------------')
+    
+    data = {'num_qubits':[], 'obj_count':[], 'circ_count':[], 'iter_count':[], 'err':[], 'params':[], 'q_sol':[], 'cl_sol':[]}
+    
     for num_qubits in tqdm(num_qubits_list):
         print('-------------------------')
         print('num_qubits:', num_qubits)
+        
+        # set oracle for f vector
         oracle_f = QuantumCircuit(num_qubits)
         oracle_f.x(num_qubits-1)
         oracle_f.h(oracle_f.qubits)
+    
+        # set vqa instance
         vqa = VQAforPoisson(num_qubits, num_layers, bc, oracle_f=oracle_f, qinstance=qins)
-        obj_counts, circ_counts, iter_counts, err, params, q_sol = [], [], [], [], [], []
+
+        obj_counts = []
+        circ_counts = []
+        iter_counts = []
+        err = []
+        params = []
+        q_sol = []
+    
         for seed in range(num_trials):
+        
             np.random.seed(seed)
+        
             x0 = list(4*np.pi*np.random.rand(vqa.num_params))
             res = vqa.minimize(x0, method='bfgs', save_logs=True)
+    
             obj_counts.append(vqa.objective_counts)
             circ_counts.append(vqa.circuit_counts)
             iter_counts.append(len(vqa.objective_count_logs))
-            trace_val = np.real(vqa.get_errors(res['x'])['trace'])
-            if trace_val >= 0:
-                err.append(np.sqrt(1 - trace_val))
-            else:
-                err.append(np.nan)
+            err.append(vqa.get_errors(res['x'])['trace'])
             params.append(res['x'])
             q_sol.append(vqa.get_sol(res['x']).real)
+            
             print('trial:', seed, 'Err.:', err[-1])
+        
         data['num_qubits'].append(num_qubits)
         data['obj_count'].append(obj_counts)
         data['circ_count'].append(circ_counts)
@@ -42,6 +58,7 @@ def experiment(bc, num_trials, num_qubits_list, num_layers, qins):
         data['params'].append(params)
         data['q_sol'].append(q_sol)
         data['cl_sol'].append(vqa.get_cl_sol().real)
+        
     return data
 
 t0 = time.time()
@@ -50,9 +67,13 @@ num_layers = 5
 num_trials = 1
 num_qubits_list = [4]
 qins = QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=42)
+
 data_p = experiment('Periodic', num_trials, num_qubits_list, num_layers, qins)
 
+
+
 def plot_solution_vectors(q_sol, cl_sol):
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(q_sol, label='quantum', color='black')
@@ -60,15 +81,27 @@ def plot_solution_vectors(q_sol, cl_sol):
     ax.legend()
     ax.set_xlabel('Node number')
     ax.set_ylabel('Components of solution')
+
     cnorm = np.linalg.norm(q_sol)
     qnorm = np.linalg.norm(cl_sol)
+
     ax.text(0.55, 0.65, 'Norm (quantum) = %.1f'%(qnorm), transform=ax.transAxes)
     ax.text(0.55, 0.55, 'Norm (classical) = %.1f'%(cnorm), transform=ax.transAxes)
+    
     return fig, ax
 
+
 idx1, idx2 = 0, 0
+
+
+
 print('Periodic boundary condition, num_qubits:', data_p['num_qubits'][idx1])
 q_sol = data_p['q_sol'][idx1][idx2]
 cl_sol = data_p['cl_sol'][idx1]
-plot_solution_vectors(q_sol, cl_sol)
+fig, ax = plot_solution_vectors(q_sol, cl_sol)
+directory = os.path.join(os.path.dirname(os.path.dirname(__file__)),'Images')
+if not os.path.exists(directory):
+    os.makedirs(directory)
+plt.savefig(os.path.join(directory, 'quantum_solution.png'))
+
 print('elapsed time: %.2e'%(time.time() - t0))
