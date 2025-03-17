d3abdb956a3d181cc96dc43adbfd9abecd6023f0
farmerp <farmerp@tcd.ie>
2025-03-17 17:20:17 +0000

    |||

--- linear_pde_solver/quantum_linear_pde_solver.py
+++ linear_pde_solver/quantum_linear_pde_solver.py
@@ -11,38 +11,35 @@ class QuantumGroundStateFinder:
         self.coefficient_set = coefficient_set
         self.gate_set = gate_set
         
-    def apply_fixed_ansatz(self, qubits, parameters):
-        circ = QuantumCircuit(3)
-        for iz in range(0, len(qubits)):
+    def apply_fixed_ansatz(self, circ, qubits, parameters):
+        for iz in range(len(qubits)):
             circ.ry(parameters[0][iz], qubits[iz])
         circ.cz(qubits[0], qubits[1])
         circ.cz(qubits[2], qubits[0])
-        for iz in range(0, len(qubits)):
+        for iz in range(len(qubits)):
             circ.ry(parameters[1][iz], qubits[iz])
         circ.cz(qubits[1], qubits[2])
         circ.cz(qubits[2], qubits[0])
-        for iz in range(0, len(qubits)):
+        for iz in range(len(qubits)):
             circ.ry(parameters[2][iz], qubits[iz])
         return circ
 
     def had_test(self, gate_type, qubits, auxiliary_index, parameters):
-        q_reg = QuantumRegister(4)
-        circ = QuantumCircuit(q_reg)
+        qreg = QuantumRegister(4)
+        circ = QuantumCircuit(qreg)
         circ.h(auxiliary_index)
-        ansatz_circ = self.apply_fixed_ansatz(qubits, parameters)
-        circ.compose(ansatz_circ, qubits, inplace=True)
-        for ie in range(0, len(gate_type[0])):
-            if (gate_type[0][ie] == 1):
+        self.apply_fixed_ansatz(circ, qubits, parameters)
+        for ie in range(len(gate_type[0])):
+            if gate_type[0][ie] == 1:
                 circ.cz(auxiliary_index, qubits[ie])
-        for ie in range(0, len(gate_type[1])):
-            if (gate_type[1][ie] == 1):
+        for ie in range(len(gate_type[1])):
+            if gate_type[1][ie] == 1:
                 circ.cz(auxiliary_index, qubits[ie])
         circ.h(auxiliary_index)
         return circ
 
-    def control_fixed_ansatz(self, qubits, parameters, auxiliary, reg):
-        circ = QuantumCircuit(reg)
-        for i in range(0, len(qubits)):
+    def control_fixed_ansatz(self, circ, qubits, parameters, auxiliary):
+        for i in range(len(qubits)):
             circ.cry(parameters[0][i], auxiliary, qubits[i])
         circ.ccx(auxiliary, qubits[1], 4)
         circ.cz(qubits[0], 4)
@@ -50,7 +47,7 @@ class QuantumGroundStateFinder:
         circ.ccx(auxiliary, qubits[0], 4)
         circ.cz(qubits[2], 4)
         circ.ccx(auxiliary, qubits[0], 4)
-        for i in range(0, len(qubits)):
+        for i in range(len(qubits)):
             circ.cry(parameters[1][i], auxiliary, qubits[i])
         circ.ccx(auxiliary, qubits[2], 4)
         circ.cz(qubits[1], 4)
@@ -58,26 +55,24 @@ class QuantumGroundStateFinder:
         circ.ccx(auxiliary, qubits[0], 4)
         circ.cz(qubits[2], 4)
         circ.ccx(auxiliary, qubits[0], 4)
-        for i in range(0, len(qubits)):
+        for i in range(len(qubits)):
             circ.cry(parameters[2][i], auxiliary, qubits[i])
         return circ
 
-    def control_b(self, auxiliary, qubits):
-        circ = QuantumCircuit(4)
+    def control_b(self, circ, auxiliary, qubits):
         for ia in qubits:
             circ.ch(auxiliary, ia)
         return circ
 
-    def special_had_test(self, gate_type, qubits, auxiliary_index, parameters, reg):
-        circ = QuantumCircuit(reg)
+    def special_had_test(self, gate_type, qubits, auxiliary_index, parameters):
+        qreg = QuantumRegister(5)
+        circ = QuantumCircuit(qreg)
         circ.h(auxiliary_index)
-        ansatz_circ = self.control_fixed_ansatz(qubits, parameters, auxiliary_index, reg)
-        circ.compose(ansatz_circ, inplace=True)
-        for ty in range(0, len(gate_type)):
-            if (gate_type[ty] == 1):
+        self.control_fixed_ansatz(circ, qubits, parameters, auxiliary_index)
+        for ty in range(len(gate_type)):
+            if gate_type[ty] == 1:
                 circ.cz(auxiliary_index, qubits[ty])
-        b_circ = self.control_b(auxiliary_index, qubits)
-        circ.compose(b_circ, inplace=True)
+        self.control_b(circ, auxiliary_index, qubits)
         circ.h(auxiliary_index)
         return circ
 
@@ -85,11 +80,10 @@ class QuantumGroundStateFinder:
         parameters = [parameters[0:3], parameters[3:6], parameters[6:9]]
         overall_sum_1 = 0
         
-        # First part of the cost function
-        for i in range(0, len(self.gate_set)):
-            for j in range(0, len(self.gate_set)):
-                multiply = self.coefficient_set[i] * self.coefficient_set[j]
-                circ = self.had_test([self.gate_set[i], self.gate_set[j]], [1, 2, 3], 0, parameters)
+        # First part of cost function
+        for i in range(len(self.gate_set)):
+            for j in range(len(self.gate_set)):
+                circ = self.had_test([self.gate_set[i], self.gate_set[j]], [1,2,3], 0, parameters)
                 backend = Aer.get_backend('aer_simulator')
                 circ.save_statevector()
                 t_circ = transpile(circ, backend)
@@ -98,19 +92,18 @@ class QuantumGroundStateFinder:
                 result = job.result()
                 outputstate = np.real(result.get_statevector(circ, decimals=100))
                 m_sum = sum(outputstate[l]**2 for l in range(1, len(outputstate), 2))
-                overall_sum_1 += multiply * (1 - 2*m_sum)
+                overall_sum_1 += self.coefficient_set[i] * self.coefficient_set[j] * (1 - 2*m_sum)
 
-        # Second part of the cost function
+        # Second part of cost function
         overall_sum_2 = 0
-        for i in range(0, len(self.gate_set)):
-            for j in range(0, len(self.gate_set)):
-                multiply = self.coefficient_set[i] * self.coefficient_set[j]
+        for i in range(len(self.gate_set)):
+            for j in range(len(self.gate_set)):
                 mult = 1
-                for extra in range(0, 2):
+                for extra in range(2):
                     if extra == 0:
-                        circ = self.special_had_test(self.gate_set[i], [1, 2, 3], 0, parameters, QuantumRegister(5))
+                        circ = self.special_had_test(self.gate_set[i], [1,2,3], 0, parameters)
                     else:
-                        circ = self.special_had_test(self.gate_set[j], [1, 2, 3], 0, parameters, QuantumRegister(5))
+                        circ = self.special_had_test(self.gate_set[j], [1,2,3], 0, parameters)
                     backend = Aer.get_backend('aer_simulator')
                     circ.save_statevector()
                     t_circ = transpile(circ, backend)
@@ -120,17 +113,17 @@ class QuantumGroundStateFinder:
                     outputstate = np.real(result.get_statevector(circ, decimals=100))
                     m_sum = sum(outputstate[l]**2 for l in range(1, len(outputstate), 2))
                     mult *= (1 - 2*m_sum)
-                overall_sum_2 += multiply * mult
+                overall_sum_2 += self.coefficient_set[i] * self.coefficient_set[j] * mult
 
         cost = 1 - float(overall_sum_2 / overall_sum_1)
-        print(f"Current cost: {cost}")
+        print(f"Cost: {cost}")
         return cost
 
     def run_optimization(self):
         initial_params = [random.uniform(0, 3) for _ in range(9)]
-        result = minimize(self.calculate_cost_function, 
-                         x0=initial_params, 
-                         method="COBYLA", 
+        result = minimize(self.calculate_cost_function,
+                         x0=initial_params,
+                         method="COBYLA",
                          options={'maxiter': 200})
         return result
 
@@ -142,7 +135,7 @@ def main():
         gate_set=[[0, 0, 0], [0, 0, 1]]
     )
     result1 = finder1.run_optimization()
-    print("\nFirst optimization result:", result1)
+    print("\nFirst result:", result1.x)
 
     # Second configuration
     print("\nRunning second optimization...")
@@ -151,7 +144,7 @@ def main():
         gate_set=[[0, 0, 0], [0, 1, 0], [0, 0, 1]]
     )
     result2 = finder2.run_optimization()
-    print("\nSecond optimization result:", result2)
+    print("\nSecond result:", result2.x)
 
 if __name__ == "__main__":
     main()
