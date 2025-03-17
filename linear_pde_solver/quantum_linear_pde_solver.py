9d1ce2f9615962085e3a8de8584ec0bde6dc1061
farmerp <farmerp@tcd.ie>
2025-03-17 16:24:03 +0000

    Removed unwanted code

--- linear_pde_solver/quantum_linear_pde_solver.py
+++ linear_pde_solver/quantum_linear_pde_solver.py
@@ -1,56 +1,138 @@
-import qiskit
-from qiskit import QuantumCircuit, Aer, transpile, assemble
+from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import Parameter
+
+from qiskit import Aer
+backend = Aer.get_backend('statevector_simulator')
+
+
+
 import numpy as np
-from scipy.optimize import minimize
-import random
+from scipy import sparse
+from scipy.sparse import diags
+from scipy.sparse.linalg import eigs
+import matplotlib.pyplot as plt
+plt.rcParams.update({"font.size": 16})  # enlarge matplotlib fonts
 
-def apply_fixed_ansatz(qubits, parameters, circ):
-    for iz in range(len(qubits)):
-        circ.ry(parameters[0][iz], qubits[iz])
-    circ.cz(qubits[0], qubits[1])
-    circ.cz(qubits[2], qubits[0])
-    for iz in range(len(qubits)):
-        circ.ry(parameters[1][iz], qubits[iz])
-    circ.cz(qubits[1], qubits[2])
-    circ.cz(qubits[2], qubits[0])
-    for iz in range(len(qubits)):
-        circ.ry(parameters[2][iz], qubits[iz])
+from qiskit.opflow import MatrixOp
 
-def run_quantum_simulation(parameters, coefficient_set):
-    circ = QuantumCircuit(3, 3)
-    apply_fixed_ansatz([0, 1, 2], parameters, circ)
-    circ.save_statevector()
-    backend = Aer.get_backend('aer_simulator')
-    t_circ = transpile(circ, backend)
-    qobj = assemble(t_circ)
-    job = backend.run(qobj)
-    result = job.result()
-    return result.get_statevector(circ, decimals=10)
 
-def run_classical_simulation(coefficient_set):
-    a1 = coefficient_set[1] * np.array([[1,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0],
-                                         [0,0,1,0,0,0,0,0], [0,0,0,1,0,0,0,0],
-                                         [0,0,0,0,-1,0,0,0], [0,0,0,0,0,-1,0,0],
-                                         [0,0,0,0,0,0,-1,0], [0,0,0,0,0,0,0,-1]])
-    a2 = coefficient_set[0] * np.array([[1,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0],
-                                         [0,0,1,0,0,0,0,0], [0,0,0,1,0,0,0,0],
-                                         [0,0,0,0,1,0,0,0], [0,0,0,0,0,1,0,0],
-                                         [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,1]])
-    a3 = np.add(a1, a2)
-    b = np.array([float(1/np.sqrt(8))] * 8)
-    return (b.dot(a3.dot(b)) / np.linalg.norm(a3.dot(b))) ** 2
 
-def compare_quantum_vs_classical(opt_parameters, coefficient_set):
-    quantum_result = run_quantum_simulation(opt_parameters, coefficient_set)
-    classical_result = run_classical_simulation(coefficient_set)
-    print("Quantum Computation Result:", np.linalg.norm(quantum_result))
-    print("Classical Computation Result:", classical_result)
-    print("Difference:", abs(np.linalg.norm(quantum_result) - classical_result))
+total_res = {}
 
-# Optimization setup
-coefficient_set = [0.55, 0.45]
-out = minimize(lambda params: run_quantum_simulation([params[:3], params[3:6], params[6:9]], coefficient_set),
-               x0=[random.uniform(0, 3) for _ in range(9)], method="COBYLA", options={'maxiter': 200})
+N = 4   # number of qubits
+dim = 2**N # dimension of the operator A
 
-opt_parameters = [out['x'][0:3], out['x'][3:6], out['x'][6:9]]
-compare_quantum_vs_classical(opt_parameters, coefficient_set)
+# Setup a tridiagonal matrix
+k = [np.ones(dim-1), -2*np.ones(dim), np.ones(dim-1)]
+offset = [-1, 0, 1]
+A = diags(k,offset).toarray()
+
+# Setup the driving term f(x) = x
+b = np.linspace(0, 1, dim)
+h = 1/(dim-1)
+sampled_b = b*(h**2)
+bt = np.linspace(0, 1, dim)
+
+# Setup the Dirichlet B.C.s
+phi_a, phi_b = 0, 0
+sampled_b[0] -= phi_a
+sampled_b[dim-1] -= phi_b
+norm = np.linalg.norm(sampled_b)
+sampled_b = sampled_b/norm
+
+# Solve the linear system of equations
+x = np.linalg.solve(A, sampled_b)
+f = np.linalg.norm(x)
+x = x/f
+
+# Build Hamiltonian
+sampled_b = sampled_b.reshape([dim, 1])
+Hamiltonian = A@(np.eye(dim)- sampled_b@sampled_b.T)@A
+# print(Hamiltonian)
+
+print("Classical solution:\n", x)
+eig_val, eig_state = np.linalg.eig(Hamiltonian)
+# print("Eigenvalues:\n", eig_val)
+# print(min(eig_val))
+vec = eig_state[:,-1]
+# print(eig_state)
+print("Eigenvector:\n", -vec)
+
+# Transform into Pauli operators
+H_op = MatrixOp(Hamiltonian).to_pauli_op()
+print("Lenth of Pauli String:",len(H_op))
+# print(H_op)
+
+
+
+from qiskit.circuit.library import EfficientSU2
+
+depth = 5 # depth of ansatz
+ansatz = EfficientSU2(N, entanglement='linear', reps=depth, skip_final_rotation_layer=True).decompose()
+ansatz.draw(fold=300)
+
+
+
+ansatz_opt = transpile(ansatz, backend=backend, optimization_level=3)
+
+print('number and type of gates in the cirucit:', ansatz_opt.count_ops())
+print('number of parameters in the circuit:', ansatz_opt.num_parameters)
+ansatz_opt.draw(fold=300)
+
+from qiskit.algorithms.optimizers import SPSA, COBYLA, L_BFGS_B, NELDER_MEAD, SLSQP, ADAM, AQGD, CG, POWELL, QNSPSA
+
+# optimizer = SPSA(maxiter=500)
+optimizer  = L_BFGS_B(maxiter=5000)
+# optimizer  = ADAM(maxiter=200, lr=0.2)
+# optimizer  = AQGD(maxiter=1000, eta=1.0, tol=1e-06, momentum=0.25, param_tol=1e-06, averaging=10)
+# optimizer  = POWELL()
+# optimizer  = COBYLA(maxiter=10000)
+# optimizer  = SLSQP(maxiter=10000)
+
+
+
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import VQE
+
+quantum_instance = QuantumInstance(backend=backend, seed_simulator=28, seed_transpiler=28,
+                                        basis_gates=None,
+                                        optimization_level=3)
+
+
+
+
+best_result = 99999
+
+vqe = VQE(ansatz_opt, optimizer,quantum_instance=quantum_instance,initial_point=2*np.pi*np.random.rand(ansatz_opt.num_parameters))
+result = vqe.compute_minimum_eigenvalue(H_op)
+quantum_solution = -1*np.abs(result.eigenstate).real
+print(quantum_solution)
+
+if result.eigenvalue.real < best_result:
+    best_result = result.eigenvalue.real
+    kept_result = result
+print("Current round using ansatz TwoLocal with depth {}, found eigenvalue {}. Best so far {}".format(depth, result.eigenvalue.real,best_result))
+total_res.update({(N, depth):kept_result})
+
+t = np.arange(0., 1., 0.02)
+res = (t**3-t)/6
+norm_res = np.linalg.norm(res)
+res_norm = res/norm_res
+
+xt = np.arange(0,1,1/dim)
+exact = [1/6*(x**3-x) for x in np.arange(0,1,1/dim)]
+norm = np.linalg.norm(exact)
+exact = exact/norm
+
+# red dashes, blue squares and green triangles
+plt.plot(t, res_norm, 'r-', label='analytical')
+plt.plot(bt, x, 'o-.', label='classical')
+plt.plot(bt, quantum_solution, 'gx--', label='quantum')
+# plt.legend()
+plt.legend(loc="lower left")
+plt.xlabel('Boundary [0,1]')
+plt.ylabel('Solution Profile')
+plt.title("4-qubit VQE for Poisson Eqn, TwoLocal, BFGS")
+plt.grid(linestyle = '--', linewidth = 0.5)
+# plt.show()
+# plt.savefig("Poisson.png", bbox_inches='tight', dpi=300)
